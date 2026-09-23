9deb9f5a6792607e694a087eb763c66057ebd95d
enriq <enriq@KIKELAPTOP>
2026-09-23 14:13:43 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyGestWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

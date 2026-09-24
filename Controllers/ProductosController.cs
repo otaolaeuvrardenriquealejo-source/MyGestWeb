13adf4d60993618fd0dc682d2b0cb9f1a2d1a4b6
otaolaeuvrardenriquealejo-source <otaolaeuvrard.enriquealejo@usal.edu.ar>
2026-09-24 17:15:36 -0300

    Modulo de productos

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,150 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyGestWeb.Models;
+using MyGestWeb.Data;
+
+public class ProductosController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ProductosController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: PRODUCTOS
+    public async Task<IActionResult> Index()    
+    {
+        return View(await _context.Productos.ToListAsync());
+    }
+
+    // GET: PRODUCTOS/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var producto = await _context.Productos
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (producto == null)
+        {
+            return NotFound();
+        }
+
+        return View(producto);
+    }
+
+    // GET: PRODUCTOS/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: PRODUCTOS/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Nombre,Stock,Precio")] Producto producto)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(producto);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(producto);
+    }
+
+    // GET: PRODUCTOS/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var producto = await _context.Productos.FindAsync(id);
+        if (producto == null)
+        {
+            return NotFound();
+        }
+        return View(producto);
+    }
+
+    // POST: PRODUCTOS/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int? id, [Bind("Id,Nombre,Stock,Precio")] Producto producto)
+    {
+        if (id != producto.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(producto);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductoExists(producto.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(producto);
+    }
+
+    // GET: PRODUCTOS/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var producto = await _context.Productos
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (producto == null)
+        {
+            return NotFound();
+        }
+
+        return View(producto);
+    }
+
+    // POST: PRODUCTOS/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int? id)
+    {
+        var producto = await _context.Productos.FindAsync(id);
+        if (producto != null)
+        {
+            _context.Productos.Remove(producto);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool ProductoExists(int? id)
+    {
+        return _context.Productos.Any(e => e.Id == id);
+    }
+}
